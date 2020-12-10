032f8fa6290fea971cc8d00ec13ccac782e3abc5
pulkit99dev <pulkitnagpal987654321@gmail.com>
2020-12-10 19:30:40 +0530

    hosted basic server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express');
 const port = 9000;
+
+let app = express();
+
+app.listen(port, function(err){
+    if(err){
+        console.log('Failed to create server');
+    }
+    console.log(`Server is up & running at port ${port}`);
+})
